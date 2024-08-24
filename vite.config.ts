be75235ba17cee3857e2046f62509b8b26a84fce
Micha Buchholz <micha@haus23.net>
2024-08-24 23:44:20 +0200

    feat: Add remix-flat-routes to generate routes.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite';
 
+// Vite plugins
 import { vitePlugin as remix } from '@remix-run/dev';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+// Remix tools
+import { flatRoutes } from 'remix-flat-routes';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     remix({
+      ignoredRouteFiles: ['**/*'],
+      routes: async (defineRoutes) => {
+        return flatRoutes('routes', defineRoutes);
+      },
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
