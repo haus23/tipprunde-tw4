779e76c8e2a7c73bc0adc7fc52fb01bcc8945dd8
Micha Buchholz <micha@haus23.net>
2024-08-24 21:26:25 +0200

    chore: Switch to tailwindcss v4.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
+
+import { vitePlugin as remix } from '@remix-run/dev';
+import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     remix({
       future: {
         v3_fetcherPersist: true,
