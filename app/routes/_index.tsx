6ca0cb2634de54bea0e4a89fad51a500f6d884d4
Micha Buchholz <micha@haus23.net>
2024-08-24 21:50:10 +0200

    feat: Start tipprunde project.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,47 +2,15 @@ import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'New Remix App' },
-    { name: 'description', content: 'Welcome to Remix!' },
+    { title: 'runde.tips' },
+    { name: 'description', content: 'Tipprunde der Marie 23' },
   ];
 };
 
 export default function Index() {
   return (
     <div className="p-4 font-sans">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="mt-4 list-disc space-y-2 pl-6">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1 className="text-3xl">runde.tips</h1>
     </div>
   );
 }
