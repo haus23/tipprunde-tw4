afb1b7d4f833c8af6ac3847f1df9a5c71085df65
Micha Buchholz <micha@haus23.net>
2024-08-25 21:35:03 +0200

    feat: Add login via static manager passwd.

--- app/routes/_foh+/_auth+/login.tsx
+++ app/routes/_foh+/_auth+/login.tsx
@@ -1,5 +1,10 @@
-import type { LoaderFunction, MetaFunction } from '@remix-run/node';
-import { requireAnonymous } from '~/utils/.server/auth';
+import type {
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
+  MetaFunction,
+} from '@remix-run/node';
+import { Form, useActionData } from '@remix-run/react';
+import { login, requireAnonymous } from '~/utils/.server/auth';
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,16 +13,31 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export const loader: LoaderFunction = async ({ request }) => {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
   await requireAnonymous(request);
-
   return null;
 };
 
+export const action = async ({ request }: ActionFunctionArgs) => {
+  return await login(request);
+};
+
 export default function LoginRoute() {
+  const actionData = useActionData<typeof action>();
+
   return (
     <div className="p-4">
       <h2 className="text-2xl">Log In</h2>
+      <Form method="post">
+        <fieldset>
+          <div className="flex flex-col gap-y-2 py-4">
+            <label htmlFor="passwd">Manager Passwort</label>
+            <input type="password" name="passwd" id="passwd" />
+          </div>
+          {actionData?.errors && <div>{actionData.errors.passwd}</div>}
+          <button type="submit">Anmelden</button>
+        </fieldset>
+      </Form>
     </div>
   );
 }
--- app/utils/.server/auth.ts
+++ app/utils/.server/auth.ts
@@ -1,7 +1,40 @@
+import { randomUUID } from 'node:crypto';
 import { redirect } from '@remix-run/node';
-import { getSession } from './auth.session';
+import { commitSession, getSession } from './auth.session';
 
 const validSessions: string[] = [];
+
+/**
+ * Performs user login
+ *
+ * Expects manager password in the request body.
+ * Returns error for invalid password. Otherwise redirects to manager dashboard.
+ *
+ * @param request Request object
+ */
+export async function login(request: Request) {
+  const session = await getSession(request);
+
+  const formData = await request.formData();
+  const password = String(formData.get('passwd'));
+
+  if (password !== process.env.MANAGER_PASSWD) {
+    return {
+      errors: { passwd: 'Falsches Passwort.' },
+    };
+  }
+
+  // Create Server Session
+  const sessionId = randomUUID();
+  validSessions.push(sessionId);
+  session.set('sessionId', sessionId);
+
+  throw redirect('/manager', {
+    headers: {
+      'Set-Cookie': await commitSession(session),
+    },
+  });
+}
 
 export async function requireAnonymous(request: Request) {
   const session = await getSession(request);
